583299727a10d0ce05c33336dae7860338f90e50
Slavikg <slavikg-g@yandex.ru>
2015-09-20 14:46:12 +0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,6 @@ class User < ActiveRecord::Base
 						uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+	validates :password_confirmation, length: { minimum: 6 }
 
 end
